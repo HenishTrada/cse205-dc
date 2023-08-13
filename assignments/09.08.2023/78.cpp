3e6b912c9142ff74f3df1aeb5afbce631d96e16c
Henish_t <118294785+HenishTrada@users.noreply.github.com>
2023-08-13 21:20:12 +0530

    Update 78.cpp

--- assignments/09.08.2023/78.cpp
+++ assignments/09.08.2023/78.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> sub_result;
-        subsets(nums, 0, sub_result, result);
-        return result;
-    }
-private:
-    void subsets(vector<int>& nums, int i, vector<int>& sub_result, vector<vector<int>>& result) {
-        result.push_back(sub_result);
-        for (int j = i; j < nums.size(); j++) {
-            sub_result.push_back(nums[j]);
-            subsets(nums, j + 1, sub_result, result);
-            sub_result.pop_back();
+    vector<vector<int>> ans;
+    void solve(vector<int> nums, int  i, vector<int>temp){
+        if(i == nums.size()){
+            ans.push_back(temp);
+            return;
         }
+        temp.push_back(nums[i]);
+        solve(nums, i+1, temp);
+        temp.pop_back();
+        solve(nums, i+1, temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<int> temp;
+        solve(nums, 0, temp);
+        return ans;
     }
 };
