f5d37bc0fa62caa1a6226216919a7339fa633279
Henish Trada <tradahenish94@gmail.com>
2023-08-17 21:51:49 +0530

    commit combination sum

---
+++ assignments/16.08.2023/39.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+void solve(vector<int>& candidates, int target,vector<int>& temp,vector<vector<int>>& ans, int i){
+    if(i== candidates.size()){
+        if(target == 0){
+            ans.push_back(temp);
+        }
+        return;
+    }
+    if(candidates[i] <= target){
+        temp.push_back(candidates[i]);
+        solve(candidates, target - candidates[i], temp,ans, i);
+        temp.pop_back();
+    }
+        solve(candidates, target, temp,ans, i+1);
+
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        solve(candidates, target, temp,ans, 0);
+        return ans;
+    }
+};
